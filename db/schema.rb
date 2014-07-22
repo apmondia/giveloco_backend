c64a55ad7f0a0ab52481d37d2a170749885762d3
Ben Scott <ben@goodbomb.com>
2014-07-22 16:50:24 -0700

    Initial users data model and db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140722231406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.text     "type"
+    t.text     "first_name"
+    t.text     "last_name"
+    t.text     "company_name"
+    t.text     "email"
+    t.text     "password"
+    t.text     "password_salt"
+    t.text     "street_address"
+    t.text     "city"
+    t.text     "state"
+    t.text     "country"
+    t.text     "zip"
+    t.text     "tags"
+    t.text     "summary"
+    t.text     "description"
+    t.text     "website"
+    t.decimal  "balance",                      precision: 15, scale: 2
+    t.integer  "total_debits"
+    t.decimal  "total_debits_value",           precision: 15, scale: 2
+    t.integer  "total_credits"
+    t.decimal  "total_credits_value",          precision: 15, scale: 2
+    t.boolean  "is_featured"
+    t.integer  "supporters"
+    t.integer  "supported_causes"
+    t.integer  "vouchers"
+    t.integer  "transactions"
+    t.integer  "redemptions"
+    t.boolean  "is_authenticated"
+    t.string   "profile_picture_file_name"
+    t.string   "profile_picture_content_type"
+    t.integer  "profile_picture_file_size"
+    t.datetime "profile_picture_updated_at"
+    t.datetime "last_login"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
