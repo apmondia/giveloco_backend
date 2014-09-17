c8c1a124a95fc0fe37a56f162933452acf9cb2eb
Ben Scott <ben@goodbomb.com>
2014-09-17 14:45:20 -0700

    Indexed user IDs

--- db/schema.rb
+++ db/schema.rb
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20140818042106) do
   add_index "users", ["company_name"], name: "company_name_index", unique: true, using: :btree
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["id"], name: "index_users_on_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
