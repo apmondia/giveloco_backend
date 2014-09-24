682cc6e647f79d8df3f96a51af10e6bd4c4d6f7a
Ben Scott <ben@goodbomb.com>
2014-09-24 10:30:32 -0700

    Fixed authenticate function

--- app/api/v1/helpers.rb
+++ app/api/v1/helpers.rb
@@ -3,13 +3,19 @@ module V1::Helpers
 	def current_user
 		env['warden'].user
 	end
-	
-	def authenticate!
-		@request_user = User.find(params[:id])
-		@auth_token = @request_user.authentication_token
-		@session_token = request.headers['X-Session-Token']
 
-		error!('Unauthorized', 401) unless @session_token == @auth_token
+	# Authenticate user by auth_token and request header
+	def authenticate!
+		@session_token = request.headers['X-Session-Token']
+		@request_user = User.find(params[:id])
+
+		if @request_user.authentication_token == nil
+			@auth_token = nil
+		else
+			@auth_token = @request_user.authentication_token
+		end
+		
+		error!('Unauthorized', 401) unless @session_token == @auth_token && @auth_token != nil
 	end
 
 	# Define Strong Parameters for Rails app
