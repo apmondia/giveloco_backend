a052499f344843e3ce600a25484524cc7408cc36
Ben Scott <ben@goodbomb.com>
2014-10-07 13:27:46 -0700

    Fixed some JSON entities

--- app/api/v1/transactions/entities.rb
+++ app/api/v1/transactions/entities.rb
@@ -7,17 +7,19 @@ module V1
 
 			expose :id, :documentation => {:type => "integer", :desc => "The database ID of the transaction."}
 	    	expose :trans_type, :documentation => {:type => "string", :desc => "The type of transaction. Can be PLEDGE, DONATION, or REDEMPTION."}
-	    	expose :trans_id, :documentation => {:type => "integer", :desc => "The numeric ID of the transaction."}
-	    	expose :stripe_transaction_id, :documentation => {:type => "string", :desc => "The numeric ID of the Braintree Payments transaction (if one exists)."}
-			expose :amount, :documentation => {:type => "decimal", :desc => "The financial value of the transaction"}
-			expose :from_user_id, :documentation => {:type => "integer", :desc => "The ID of the user who started the transaction."}
-	    	expose :to_user_id, :documentation => {:type => "integer", :desc => "The ID of the user who accepted the transaction."}
-	    	expose :from_name, :documentation => {:type => "string", :desc => "The NAME of the user who started the transaction."}
-	    	expose :to_name, :documentation => {:type => "string", :desc => "The NAME of the user who accepted the transaction."}
-	    	expose :from_user_role, :documentation => {:type => "string", :desc => "The ROLE of the user who started the transaction."}
-	    	expose :to_user_role, :documentation => {:type => "string", :desc => "The ROLE of the user who accepted the transaction."}
-	    	expose :from_user_balance, :documentation => {:type => "decimal", :desc => "The running balance of the user who STARTED the transaction"}
-	    	expose :to_user_balance, :documentation => {:type => "decimal", :desc => "The running balance of the user who ACCEPTED the transaction"}
+	    	with_options(if: {:type => 'authorized'}) do
+		    	expose :trans_id, :documentation => {:type => "integer", :desc => "The numeric ID of the transaction."}
+		    	expose :stripe_transaction_id, :documentation => {:type => "string", :desc => "The numeric ID of the Braintree Payments transaction (if one exists)."}
+				expose :amount, :documentation => {:type => "decimal", :desc => "The financial value of the transaction"}
+				expose :from_user_id, :documentation => {:type => "integer", :desc => "The ID of the user who started the transaction."}
+		    	expose :to_user_id, :documentation => {:type => "integer", :desc => "The ID of the user who accepted the transaction."}
+		    	expose :from_name, :documentation => {:type => "string", :desc => "The NAME of the user who started the transaction."}
+		    	expose :to_name, :documentation => {:type => "string", :desc => "The NAME of the user who accepted the transaction."}
+		    	expose :from_user_role, :documentation => {:type => "string", :desc => "The ROLE of the user who started the transaction."}
+		    	expose :to_user_role, :documentation => {:type => "string", :desc => "The ROLE of the user who accepted the transaction."}
+		    	expose :from_user_balance, :documentation => {:type => "decimal", :desc => "The running balance of the user who STARTED the transaction"}
+		    	expose :to_user_balance, :documentation => {:type => "decimal", :desc => "The running balance of the user who ACCEPTED the transaction"}
+	    	end
 	    	expose :status, :documentation => {:type => "string", :desc => "The status of the transaction. Can be PENDING, CANCELLED, or COMPLETED"}
 	    	with_options(format_with: :timestamp) do
 		    	expose :cancelled_at, :documentation => {:type => "datetime", :desc => "The date and time when the transaction was CANCELLED."}
--- app/api/v1/users/entities.rb
+++ app/api/v1/users/entities.rb
@@ -8,9 +8,13 @@ module V1
 
 			expose :id, :documentation => {:type => "integer", :desc => "The numeric id of the user"}
 			expose :role, :documentation => {:type => "string", :desc => "The user's type"}
-			expose :email, :documentation => {:type => "string", :desc => "The email address of the user"}, if: {:type => 'authorized'}
-			expose :first_name, :documentation => {:type => "string", :desc => "The user's First Name"}, if: {:type => 'authorized'}
-			expose :last_name, :documentation => {:type => "string", :desc => "The user's Last Name"}, if: {:type => 'authorized'}
+
+			with_options(if: {:type => 'authorized'}) do
+				expose :email, :documentation => {:type => "string", :desc => "The email address of the user"}
+				expose :first_name, :documentation => {:type => "string", :desc => "The user's First Name"}
+				expose :last_name, :documentation => {:type => "string", :desc => "The user's Last Name"}
+			end
+
 			expose :images do
 				expose :profile_picture do
 					expose :original,  :documentation => {:type => "attachment", :desc => "The URL for the user's profile picture"}
@@ -18,6 +22,7 @@ module V1
 					expose :thumb,  :documentation => {:type => "attachment", :desc => "The URL for the user's profile picture"}
 				end
 			end
+
 			expose :phone, :documentation => {:type => "string", :desc => "The phone number for the business or cause"}
 			expose :company_name, :documentation => {:type => "string", :desc => "The user's Business or Cause Name"}
 			expose :street_address, :documentation => {:type => "string", :desc => "The user's Address"}
@@ -30,26 +35,18 @@ module V1
 			expose :description, :documentation => {:type => "text", :desc => "A long form description of the business/cause"}
 			expose :website, :documentation => {:type => "string", :desc => "The business / cause's website"}
 			expose :is_published, :documentation => {:type => "boolean", :desc => "Determines if the organization is visible to the public"}
-			expose :is_featured, :documentation => {:type => "boolean", :desc => "Determines if the organization is featured on the homepage"}			
-			expose :customer_id, :documentation => {:type => "string", :desc => "The user's Stripe customer ID."}
-			expose :balance, :documentation => {:type => "decimal", :desc => "The user's current credit balance"}
-			expose :total_funds_raised, :documentation => {:type => "decimal", :desc => "The total value of donations received by a cause"}, if: lambda { |user, options| user.role == "cause" }
+			expose :is_featured, :documentation => {:type => "boolean", :desc => "Determines if the organization is featured on the homepage"}
+			expose :donors, :using => Connections::Entities, :documentation => {:type => "object", :desc => "A list of donor IDs that have supported a cause"}, if: lambda { |user, options| user.role == "cause" }
+			expose :supporters, :using => Connections::Entities, :documentation => {:type => "object", :desc => "A list of business IDs that have supported a cause"}, if: lambda { |user, options| user.role == "cause" }
+			expose :supported_causes, :using => Connections::Entities, :documentation => {:type => "object", :desc => "A list of cause IDs that a business has supported"}, if: lambda { |user, options| user.role != "cause" }
+			expose :transactions_created, :using => Transactions::Entities, :documentation => {:type => "object", :desc => "This is a list of transactions involving this user."}
+			expose :transactions_accepted, :using => Transactions::Entities, :documentation => {:type => "object", :desc => "This is a list of transactions involving this user."}
 			
 			with_options(if: {:type => 'authorized'}) do
-				expose :donors, :using => Connections::Entities, :documentation => {:type => "object", :desc => "A list of donor IDs that have supported a cause"}, if: lambda { |user, options| user.role == "cause" }
-				expose :supporters, :using => Connections::Entities, :documentation => {:type => "object", :desc => "A list of business IDs that have supported a cause"}, if: lambda { |user, options| user.role == "cause" }
-				expose :supported_causes, :using => Connections::Entities, :documentation => {:type => "object", :desc => "A list of cause IDs that a business has supported"}, if: lambda { |user, options| user.role != "cause" }
-				expose :transactions_created, :using => Transactions::Entities, :documentation => {:type => "object", :desc => "This is a list of transactions involving this user."}
-				expose :transactions_accepted, :using => Transactions::Entities, :documentation => {:type => "object", :desc => "This is a list of transactions involving this user."}
+				expose :customer_id, :documentation => {:type => "string", :desc => "The user's Stripe customer ID."}
+				expose :balance, :documentation => {:type => "decimal", :desc => "The user's current credit balance"}
+				expose :total_funds_raised, :documentation => {:type => "decimal", :desc => "The total value of donations received by a cause"}, if: lambda { |user, options| user.role == "cause" }
 				expose :authentication_token, :as => :auth_token, :documentation => {:type => "string", :desc => "The user's current authentication token"}
-			end
-			
-			with_options(unless: {:type => 'authorized'}) do
-				expose :donors, :using => Connections::Entities::SnapShot, if: lambda { |user, options| user.role == "cause" }
-				expose :supporters, :using => Connections::Entities::SnapShot, if: lambda { |user, options| user.role == "cause" }
-				expose :supported_causes, :using => Connections::Entities::SnapShot, if: lambda { |user, options| user.role != "cause" }
-				expose :transactions_created, :using => Transactions::Entities::SnapShot
-				expose :transactions_accepted, :using => Transactions::Entities::SnapShot
 			end
 			
 			with_options(format_with: :timestamp) do
