53b35ed8d2c56d7be928b91131bc860db9daef20
Ben Scott <ben@goodbomb.com>
2014-11-05 13:39:03 -0800

    Added "is_admin" helper check for API

--- app/api/v1/helpers/users_helper.rb
+++ app/api/v1/helpers/users_helper.rb
@@ -4,21 +4,21 @@ module V1::Helpers::UsersHelper
 		env['warden'].user
 	end
 
-  def cannot? *args
-    current_ability.cannot? *args
-  end
+	def cannot? *args
+		current_ability.cannot? *args
+	end
 
-  def can? *args
-    current_ability.can? *args
-  end
+	def can? *args
+		current_ability.can? *args
+	end
 
-  def current_ability
-    @current_ability ||= ::Ability.new(current_user)
-  end
+	def current_ability
+		@current_ability ||= ::Ability.new(current_user)
+	end
 
   # Authenticate user by auth_token and request header
 	def authenticate!		
-		error!('Unauthorized', 401) unless is_authenticated
+		error!('Unauthorized', 401) unless is_authenticated || is_admin
 	end
 
 	def is_authenticated
@@ -31,6 +31,12 @@ module V1::Helpers::UsersHelper
 			@auth_token = @request_user.authentication_token
 		end
 		(@session_token == @auth_token && @auth_token != nil) ? true : false
-  end
+  	end
+
+  	def is_admin
+  		@session_token = request.headers['X-Session-Token']
+  		@request_user = User.find_by_authentication_token(@session_token)
+  		@request_user.role == 'admin' ? true : false
+  	end
 
 end
