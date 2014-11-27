cd06dddcf02b6943e51ff8a52956c3568929a282
Ben Scott <ben@goodbomb.com>
2014-11-27 13:27:55 -0800

    Added donation_percentage to sponsorship snapshot

--- app/api/v1/sponsorships/snapshot.rb
+++ app/api/v1/sponsorships/snapshot.rb
@@ -4,6 +4,7 @@ module V1
       expose :id
       expose :business, :using => V1::Users::Snapshot
       expose :cause, :using => V1::Users::Snapshot
+      expose :donation_percentage
       expose :status
     end
   end
