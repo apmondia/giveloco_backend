d18a093fdeebf8e7751ca977704624e65aca802d
Brendan Asselstine <mail.asselstine@gmail.com>
2014-11-27 16:44:39 -0800

    Added a spec for 'As a cause I want to sign up'

---
+++ spec/features/cause/signup_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'As a cause I want to sign up' do
+
+  before(:each) do
+    assert_front_end_up
+  end
+
+  it 'should allow a cause to sign up' do
+
+    visit '/'
+    click_link 'Sign Up'
+    click_link 'Cause'
+    fill_in :company, :with => 'Freedom Org'
+    fill_in :first_name, :with => 'Bill'
+    fill_in :last_name, :with => 'Bob'
+    fill_in :email, :with => 'bob_fake_email@fakefake.com'
+    fill_in :password, :with => 'password'
+    find('form button[type="submit"]').click
+    old_wait_time = set_wait_time_to 10
+    expect(page).to have_content('You have successfully registered')
+    expect(page).to have_content('Freedom Org')
+    set_wait_time_to old_wait_time
+
+  end
+
+end
