3b610cc8c2ef04153fe8273933d6826987ddbbef
Brendan Asselstine <mail.asselstine@gmail.com>
2014-12-02 23:29:26 -0800

    Updated sendgrid SMTP: added domain option and turned on delivery errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,16 +75,16 @@ Rails.application.configure do
   # Email configuration settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address:              ENV["EMAIL_SERVER"],
-    port:                 587,
-    #domain:               ENV["EMAIL_DOMAIN"],
     user_name:            ENV["EMAIL_USERNAME"],
     password:             ENV["EMAIL_PASSWORD"],
-    authentication:       'plain',
+    domain:               ENV["EMAIL_DOMAIN"],
+    address:              ENV["EMAIL_SERVER"],
+    port:                 587,
+    authentication:       :plain,
     enable_starttls_auto: true
   }
 
