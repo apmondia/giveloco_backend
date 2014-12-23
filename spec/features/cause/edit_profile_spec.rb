0e45a920c91f4a55cda6d5b9966eee482a5594ec
Brendan Asselstine <mail.asselstine@gmail.com>
2014-12-23 11:29:28 -0800

    Added a feature test for cause cancelling profile edit

--- spec/features/cause/edit_profile_spec.rb
+++ spec/features/cause/edit_profile_spec.rb
@@ -6,13 +6,17 @@ describe 'As a cause I want to edit my profile' do
     @c = create(:cause)
   end
 
-  it 'should work' do
-
+  def goto_edit_details
     login(@c)
     click_profile_menu
     click_link 'My Account'
     expect(page).to have_content(@c.street_address)
     click_link 'Edit Details'
+  end
+
+  it 'should work' do
+
+    goto_edit_details
     expect(page).to have_content('Edit Cause Location')
     fill_in :userStreetAddress, :with => '1234 Evergreen Terrace'
     fill_in :userCity, :with => 'Moncton'
@@ -21,6 +25,16 @@ describe 'As a cause I want to edit my profile' do
     expect(page).to have_content('Your account information was successfully updated')
     expect(page).to have_content('Edit Details')
     expect(page).to have_content('1234 Evergreen Terrace')
+
+  end
+
+  it 'should allow the user to cancel their edits' do
+
+    goto_edit_details
+    fill_in :userCity, :with => 'asdf'
+    click_link_or_button 'Cancel'
+    expect(page).not_to have_content('asdf')
+    expect(page).not_to have_content('Editing')
 
   end
 
