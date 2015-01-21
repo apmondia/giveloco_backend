e82eeeccab15e42aef16841a136474c8cd649f69
Brendan Asselstine <mail.asselstine@gmail.com>
2015-01-21 10:24:18 -0800

    Added a test to ensure that users can search by campaign tag

--- spec/features/user/search_spec.rb
+++ spec/features/user/search_spec.rb
@@ -5,6 +5,7 @@ describe 'As an individual I want to search businesses and causes' do
   before(:each) do
     @sponsorships = create_list(:sponsorship, 10, :status => :accepted)
     @sponsorships.first.business.tag_list.add "test"
+    @sponsorships.first.business.campaign_list.add "campaign"
     @sponsorships.first.business.company_name = "Juju Jazz"
     @sponsorships.first.business.save!
     @sponsorships.last.business.tag_list.add 'foo'
@@ -21,6 +22,16 @@ describe 'As an individual I want to search businesses and causes' do
     fill_in :search_businesses, :with => "test"
     expect(page).to have_content(@sponsorships.first.business.company_name)
     expect(page).to_not have_content(@sponsorships.last.business.company_name)
+
+  end
+
+  it 'should allow the user to search businesses by campaign tag' do
+
+    visit '/'
+    click_link 'Explore'
+    fill_in :search_businesses, :with => 'campaign'
+    expect(page).to have_content(@sponsorships.first.business.company_name)
+    expect(page).not_to have_content(@sponsorships.last.business.company_name)
 
   end
 
