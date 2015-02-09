aaad793b7025d1c58ac5f60c412dbb79541aa43c
Brendan Asselstine <mail.asselstine@gmail.com>
2015-02-09 15:48:45 -0800

    Added a spec to ensure that the Explore buttons would reset the search terms.

--- spec/features/user/view_businesses_spec.rb
+++ spec/features/user/view_businesses_spec.rb
@@ -57,4 +57,29 @@ describe 'As a user I want to view the businesses' do
 
   end
 
+  it 'should reset the search criteria when the top buttons are clicked' do
+
+    visit_root
+
+    expect(page).to have_content(@b.company_name)
+    expect(page).to have_content(@b3.company_name)
+    expect(page).to have_content(@b4.company_name)
+
+    click_company(@b.company_name)
+    click_link 'foo'
+
+    expect(page).to have_content(@b.company_name)
+    expect(page).to have_content(@b3.company_name)
+    expect(page).not_to have_content(@b4.company_name)
+
+    within '.navbar-header' do
+      click_link 'Businesses'
+    end
+
+    expect(page).to have_content(@b.company_name)
+    expect(page).to have_content(@b3.company_name)
+    expect(page).to have_content(@b4.company_name)
+
+  end
+
 end
