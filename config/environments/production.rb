586edbc42d40ac1cb64e51bf36fb855e4355854d
Brendan Asselstine <mail.asselstine@gmail.com>
2015-02-13 12:28:27 -0800

    Added action_mailer.default_url_options to define the API_BASE_URL as the host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   # Rails.application.routes.default_url_options[:host] = 'yoursite.herokuapp.com'
 
   # Email configuration settings
+  config.action_mailer.default_url_options = { host: ENV["API_BASE_URL"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
